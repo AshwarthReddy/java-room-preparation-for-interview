a9b0696922460635db39388b091de6411a9507cf
Aswarth Reddy <aswartha.reddy_xeb@idfcbank.com>
2022-01-18 05:31:31 +0530

    Initial commit

---
+++ src/com/anr/Main.java
@@ -0,0 +1,51 @@
+package com.anr;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+class Dog implements Serializable {
+    String name;
+    int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+public class Main {
+
+
+
+    // serialization
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Dog dog = new Dog("Puppy", 2);
+
+        FileOutputStream fos  = new FileOutputStream("dog.ser");
+        ObjectOutputStream oos =  new ObjectOutputStream(fos);
+        oos.writeObject(dog);
+
+        // de-serialization
+        FileInputStream fis = new FileInputStream("dog.ser");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fis);
+        Dog d = (Dog) objectInputStream.readObject();
+        System.out.println("d = " + d);
+
+
+
+    }
+
+
+}
